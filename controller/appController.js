86ecbaac59a6b0572358e0045dcffd92ba5a8e3e
MohamadOjail <mohamad.ojail@gmail.com>
2022-10-13 00:22:37 +0200

    req-mod

--- controller/appController.js
+++ controller/appController.js
@@ -2,6 +2,11 @@
 var path = require('path');
 
 const testServer = (req, res) => {
+    const incoming = {
+        path: req.path,
+        host_name: req.hostname
+    }
+    console.log(incoming);
     try {
         res.status(200)
             .sendFile('alive.html', { root: path.join(__dirname, '../serverAppPages') })
@@ -13,6 +18,11 @@ const testServer = (req, res) => {
 }
 
 const sendInstructions = (req, res) => {
+    const incoming = {
+        path: req.path,
+        host_name: req.hostname
+    }
+    console.log(incoming);
     try {
         res.status(200)
             .sendFile('instructions.html', { root: path.join(__dirname, '../serverAppPages') })
